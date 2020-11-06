f73d11a9906908bf297c0762ca8226b09c467f68
Aleksandre Lomadze-Gabiani <lomadzeg@usc.edu>
2020-11-06 03:33:20 -0700

    Created initial Skeleton code
    Added classes:
    - Authenticator
    - Guest
    - User
    - Post
    - TestRunner

---
+++ src/CSCI201FinalProject/Guest.java
@@ -0,0 +1,19 @@
+package CSCI201FinalProject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Guest implements Runnable {
+	
+	public List<Post> getPosts() {
+		//TODO: Retrieve a lists of posts to display from the database
+		return new ArrayList<Post>();
+	}
+
+	@Override
+	public void run() {
+		// TODO: Write function to let Guest use functionality during runtime
+	}
+	
+	
+}
